7a9848a7caa72f644b4c544c873583fe44621ef4
LaBytex <78768489+LaBytex@users.noreply.github.com>
2021-02-24 11:57:04 +0000

    Create BytexCasinoToken.sol

---
+++ tokens/BytexCasinoToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.6;
+
+import "../BEP20Token.sol";
+
+contract BytexCasinoToken is BEP20Token {
+  constructor() public BEP20Token("Bytex Casino Token", "BBC", 18, 100 * 1e6 * 1e18) {
+  }
+}
